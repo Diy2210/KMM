92620643e11ddc50d8aff5b5e0ac488d16e21ed1
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-10-04 19:13:56 +0700

    change android gradle plugin to stable version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-        classpath("com.android.tools.build:gradle:4.1.0-rc03")
+        classpath("com.android.tools.build:gradle:4.0.1")
         classpath ("com.squareup.sqldelight:gradle-plugin:1.4.3")
     }
 }
